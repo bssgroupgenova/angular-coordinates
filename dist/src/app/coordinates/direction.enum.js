f0b69c8809c0f9ecb72ac0b61b13329de147612d
ildoc <il_doc@protonmail.com>
2020-02-10 09:48:39 +0100

    .

--- dist/src/app/coordinates/direction.enum.js
+++
@@ -1,6 +0,0 @@
-export var Direction;
-(function (Direction) {
-    Direction[Direction["Latitude"] = 1] = "Latitude";
-    Direction[Direction["Longitude"] = 2] = "Longitude";
-})(Direction || (Direction = {}));
-//# sourceMappingURL=direction.enum.js.map
